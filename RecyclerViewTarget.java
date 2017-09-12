c9957a696df8c45764734d51151b11d69e0e5c8b
DESKTOPC-Win10HD\Emre <emrecelebiiii@gmail.com>
2017-09-13 00:15:17 +0300

    ShowCaseView Library Circle Point
    RecyclerView item in childview circle point

---
+++ RecyclerViewTarget.java
@@ -0,0 +1,41 @@
+import android.graphics.Point;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+/**
+ * Created by Emre CELEBÝ on 12.09.2017. cLB
+ */
+
+public class RecyclerViewTarget {
+
+    RecyclerView recyclerView;
+    private int offsetX;
+    private int offsetY;
+
+    private int childViewByID;
+
+    public RecyclerViewTarget(RecyclerView recyclerView, int childViewByID) {
+        this.recyclerView = recyclerView;
+        this.childViewByID = childViewByID;
+    }
+
+    public RecyclerViewTarget(int viewId, int offsetX, int offsetY, RecyclerView recyclerView) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.recyclerView = recyclerView;
+    }
+
+    public Point getPoint() {
+        int[] location = new int[2];
+        View childAt = recyclerView.getChildAt(0);
+        childAt.getLocationInWindow(location);
+        int x = location[0] + (int) childAt.findViewById(childViewByID).getX() + (dpToPx(50) / 2); // --> 50dp(childview width)
+        int y = location[1] + childAt.getHeight() / 2;
+        return new Point(x, y);
+    }
+	
+	public  int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+}
